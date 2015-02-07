f9a08885cd7fb0047d2d7060a5dcf1f3c40efbc6
stvnrlly <stevenmreilly+github@gmail.com>
2015-02-07 08:31:50 -0500

    refactor to use mongo database

--- server.js
+++ server.js
@@ -1,43 +1,78 @@
 var express = require('express')
 var Twit = require('twit')
-var fs = require('fs')
+var http = require('http')
 
-try {
-  var count = require('./public/count.json')
-} catch(e) {
-  count = {"yes":0,"no":0}
-}
+// Retrieve
+var mongo = require('mongodb').MongoClient;
+var MONGODB_URI = process.env.MONGOLAB_URI || "mongodb://localhost"
+
+// Twitter credentials
+var twit = new Twit({
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET
+});
 
 var app = express()
-app.use(express.static(__dirname + '/public'))
+var port = process.env.PORT || 8080;
 
-var twit = new Twit({
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET
-});
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.listen(process.env.PORT || 8080)
+var server = http.createServer(app).listen(port, function() {
+    console.log('Express server listening on port ' + port);
+});
+var io = require('socket.io').listen(server);
 
-var stream = twit.stream('statuses/sample')
-var yes = /\w+,\s\w+,\sand\s\w+/
-var no = /\w+,\s\w+\sand\s\w+/
+// Connect to the db
+mongo.connect(MONGODB_URI, function(err, db) {
+    if(!err) {
+        console.log("Connected to MongoDB");
+    }
+    var collection = db.collection('oxford');
+    collection.findOne({'type':'yes'}, function(err, doc){
+        if(doc == null){
+            collection.insert({'type':'yes','count':0}, {w:1}, function(err, result) {})
+        }
+    })
+    collection.findOne({'type':'no'}, function(err, doc){
+        if(doc == null){
+            collection.insert({'type':'no','count':0}, {w:1}, function(err, result) {})
+        }
+    })
 
-stream.on('tweet', function (tweet) {
-  if (yes.test(tweet['text'])) {
-    count['yes'] += 1
-    console.log(tweet['text'])
-    console.log(count)
-    fs.writeFile('./public/count.json', JSON.stringify(count), 'utf8')
-  } else if (no.test(tweet['text'])) {
-    count['no'] += 1
-    console.log(tweet['text'])
-    console.log(count)
-    fs.writeFile('./public/count.json', JSON.stringify(count), 'utf8')
-  }
-})
+    var stream = twit.stream('statuses/sample')
+    var yes = /\w+,\s\w+,\sand\s\w+/
+    var no = /\w+,\s\w+\sand\s\w+/
+
+    stream.on('tweet', function (tweet) {
+        if (yes.test(tweet['text'])) {
+            console.log(tweet['text'])
+            collection.findAndModify({'type':'yes'},[[ '_id', 1]], {$inc:{'count':1}}, {new:true}, function(err, result) {
+                io.emit('yes', result['count'])
+                console.log(result['count'])
+            })
+        } else if (no.test(tweet['text'])) {
+            console.log(tweet['text'])
+            collection.findAndModify({'type':'no'},[[ '_id', 1]], {$inc:{'count':1}}, {new:true}, function(err, result) {
+                io.emit('no', result['count'])
+                console.log(result['count'])
+            })
+        }
+    })
+
+    io.on('connection', function(socket){
+        socket.on('ready', function(data){
+            collection.findOne({'type':'yes'}, {'count':true}, function(err, doc){
+                socket.emit('yes', doc['count'])
+            })
+            collection.findOne({'type':'no'}, {'count':true}, function(err, doc){
+                socket.emit('no', doc['count'])
+            })
+        });
+    })
+});
